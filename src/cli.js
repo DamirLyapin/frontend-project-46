b32d94854acaf812b07fa0a5af35691bc2cdd464
Damir <damirlyapin2001@gmail.com>
2025-12-06 14:22:45 +0300

    add option format

--- src/cli.js
+++ src/cli.js
@@ -7,6 +7,7 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .option('-f, --format [type]', 'output format')
   .argument('<filepath1>', 'path to first file')
   .argument('<filepath2>', 'path to second file')
   .action((filepath1, filepath2) => {
