b5d31498479553c7da0a5b5440bc79633f4cf7ef
Damir <damirlyapin2001@gmail.com>
2025-12-09 16:22:13 +0300

    add genDiff

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
-[![Actions Status](https://github.com/DamirLyapin/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/DamirLyapin/frontend-project-46/actions)
+[![Actions Status](https://github.com/DamirLyapin/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/DamirLyapin/frontend-project-46/actions)process of working with 2 json files:
+https://asciinema.org/a/gB3ukkewNI9rkcVykz3c39gDU
+
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,50 @@
 import { Command } from 'commander';
 import parseFile from './index.js';
 
+const sortFiles = (file) => {
+  const entries = Object.entries(file)
+  const sortedEntries = entries.sort((a, b) => {
+    if (a[0] < b[0]) {
+      return -1
+    }
+    if (a[0] > b[0]) {
+      return 1
+    }
+    return 0
+  })
+  return Object.fromEntries(sortedEntries)
+}
+
+const genDiff = (data1, data2) => {
+  let result = '{\n'
+  const sortedData1 = sortFiles(data1)
+  const sortedData2 = sortFiles(data2)
+  const allKeys = [
+    ...new Set([
+      ...Object.keys(sortedData1),
+      ...Object.keys(sortedData2)
+    ])
+  ].sort()
+  allKeys.forEach(key => {
+    const value1 = sortedData1[key]
+    const value2 = sortedData2[key]
+    if (value1 !== undefined && value2 !== undefined) {
+      if (value1 === value2) {
+        result += `  ${key}: ${value1}\n`
+      } else {
+        result += `- ${key}: ${value1}\n`
+        result += `+ ${key}: ${value2}\n`
+      }
+    } else if (value1 !== undefined) {
+      result += `- ${key}: ${value1}\n`
+    } else {
+      result += `+ ${key}: ${value2}\n`
+    }
+  })
+  result += '}'
+  return result
+}
+
 const program = new Command();
 
 program
@@ -13,8 +57,7 @@ program
     try {
       const file1 = parseFile(filepath1)
       const file2 = parseFile(filepath2)
-      console.log(`File 1: ${file1}`)
-      console.log(`File 2: ${file2}`)
+      console.log(genDiff(file1, file2))
     } catch(e) {
       console.error(`Error: ${e.message}`)
     }
